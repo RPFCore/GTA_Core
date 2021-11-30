5ac84cb62d0f7167c48f876492f3304b1aa99cc2
RPF Studio <groupe-esp-fr@outlook.fr>
2021-11-30 16:53:05 +0100

    Delete nMainNotification directory

--- nMainNotification/server_example.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("GTA_NUI_ShowNotif_server")
-AddEventHandler("GTA_NUI_ShowNotif_server", function()
-    TriggerClientEvent("GTA_NUI_ShowNotif_client", source, "Notification Hello World ahahahahah.", "success", "fas fa-check fa-2x", "row", "success")
-end)
